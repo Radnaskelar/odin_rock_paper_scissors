8b1f0159bfd920261c4ed0d3c0c0c444c03cac3f
Alex <aleksa.radisav1994@gmail.com>
2021-12-02 00:32:40 +0100

    clarity updated

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-// computer chooses from array
+// computer chooses from array, returned value is number
+// convert to string via switch()
+//
 
 const computerPlay = () => {
     const gameArray = ['rock', 'paper', 'scissors'];
@@ -21,14 +23,17 @@ const playerPlay = () => {
 }
 
 // single round takes two parameters which have values of function expressions
+// an if else conditional is used to check and compare player and computer values
 
 const gameRound = function (playerSelection, computerSelection) {
     if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
         return 'Player wins!';
     } else if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'rock')) {
         return 'Player loses!';
+    } else if (playerSelection === computerSelection) {
+        return 'Tie!';
     } else {
-        return 'Draw!';
+        return 'Invalid input!';
     }
 }
 let playerSelection = playerPlay();
